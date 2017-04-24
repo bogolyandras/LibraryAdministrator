3751fc46c22b9eccd66858c3083594b4ae75467b
Andras Bogoly <ebogoly@gmail.com>
2017-04-24 20:49:51 +0200

    Renaming main window form

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("LibraryAdministrator")>
+<Assembly: AssemblyTitle("Library Administrator")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("LibraryAdministrator")>
+<Assembly: AssemblyProduct("Library Administrator")>
 <Assembly: AssemblyCopyright("Copyright ©  2017")>
 <Assembly: AssemblyTrademark("")>
 
