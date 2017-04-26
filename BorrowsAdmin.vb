7821bd0943628179e0e0879156be5299c45e344e
Andras Bogoly <ebogoly@gmail.com>
2017-04-27 00:24:05 +0200

    Now we can take back books

--- BorrowsAdmin.vb
+++ BorrowsAdmin.vb
@@ -33,10 +33,39 @@
             cell = New DataGridViewTextBoxCell()
             cell.Value = BorrowRow("start_date")
             row.Cells.Add(cell)
+            row.Tag = BorrowRow("id")
 
             BorrowGridView.Rows.Add(row)
 
         Next
     End Sub
 
+    Private Sub takenBackButton_Click(sender As Object, e As EventArgs) Handles takenBackButton.Click
+        If BorrowGridView.SelectedCells.Count > 0 Then
+
+            Dim borrowId = BorrowGridView.SelectedCells(0).OwningRow.Tag
+
+            For Each BorrowRow In BookLendingsTableAdapter.GetData().Rows
+                If BorrowRow("id") = borrowId Then
+
+                    For Each BookRow In BooksTableAdapter.GetData().Rows
+                        If BorrowRow("book_id") = BookRow("id") Then
+                            BookRow("available") = BookRow("available") + 1
+                            BooksTableAdapter.Update(BookRow)
+                            Exit For
+                        End If
+                    Next
+
+                    BorrowRow("transaction_finished") = True
+                    BorrowRow("end_date") = DateTime.Now
+                    BookLendingsTableAdapter.Update(BorrowRow)
+                    Exit For
+                End If
+            Next
+
+            MsgBox("A kölcsönzést lezártuk.")
+            refreshResulst()
+
+        End If
+    End Sub
 End Class
